2e565b3eb24311d548f99406570e43ef34d9cb65
Muhamad Sholiudin <148538151+MSholiudin@users.noreply.github.com>
2023-10-30 21:18:09 +0700

    fix home

---
+++ app/src/main/java/com/Try/MyApps/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.Try.MyApps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.Try.MyApps.databinding.ActivitySplashscreenBinding
+import com.Try.MyApps.ui.authentification.LoginActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Ini pindah ke MainActivity_Login
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 1500) // Delay selama 3 detik
+    }
+}
